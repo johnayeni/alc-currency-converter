e86724eec50347d7aa67c9a4b8c79a32ec2ecf2c
John Ayeni <johnayeni10.aj@gmail.com>
2018-06-29 13:22:07 +0100

    fixed indexedDB error

--- js/app.js
+++ js/app.js
@@ -87,15 +87,7 @@ class ApplicationController {
           appController._fetchConversionRate();
         });
       } else {
-        appController._dbPromise.then(db => {
-          if (!db) return;
-
-          let tx = db.transaction('currencies').objectStore('currencies');
-          return tx.getAll().then(currencies => {
-            appController._addCurrenciesToView(currencies);
-            appController._fetchConversionRate();
-          });
-        });
+        appController._getCurrenciesFromIDB();
       }
     } catch (error) {
       showToast('Cannot currently get any data');
@@ -103,38 +95,36 @@ class ApplicationController {
   }
 
   // get conversion rate from API
-  _fetchConversionRate() {
+  async _fetchConversionRate() {
     const appController = this;
     const currency1 = view.currencyList1.value;
     const currency2 = view.currencyList2.value;
     const query = `${currency1.toUpperCase()}_${currency2.toUpperCase()}`;
-    try {
-      if (navigator.onLine) {
-        let promise = new Promise((resolve, reject) => {
-          return fetch(`${API_URL}/api/v5/convert?q=${query}`).then(
-            response => {
-              resolve(response.json());
-            },
+    let conversion = await appController._getConversionFromIDB(query);
+    if (!conversion) {
+      let promise = new Promise((resolve, reject) => {
+        try {
+          return fetch(`${API_URL}/api/v5/convert?q=${query}`).then(response =>
+            resolve(response.json()),
           );
-        });
-        promise.then(response => {
-          appController._applyConversionRate(
-            response.results[query].val,
-            query,
-          );
-          appController._storeConversionInIDB(response.results[query]);
-        });
-      } else {
-        appController._getConversionFromIDB(query);
-      }
-    } catch (error) {
-      showToast('Cannot currently get any data');
+        } catch (error) {
+          showToast('Could not perfrom conversion');
+        }
+      });
+      promise.then(response => {
+        appController._storeConversionInIDB(response.results[query]);
+        conversion = response.results[query];
+        appController._applyConversionRate(conversion.val, query);
+      });
+    } else {
+      appController._applyConversionRate(conversion.val, query);
     }
   }
 
   // store list of currencies on IDB
   _storeCurrenciesInIDB(currencies) {
-    this._dbPromise.then(db => {
+    const appController = this;
+    appController._dbPromise.then(db => {
       if (!db) return;
 
       let tx = db.transaction('currencies', 'readwrite');
@@ -149,6 +139,61 @@ class ApplicationController {
         store.put(currency);
       });
     });
+  }
+
+  // store a conversion rate on IDB
+  _storeConversionInIDB(conversion) {
+    const appController = this;
+    appController._dbPromise.then(db => {
+      if (!db) return;
+
+      let tx = db.transaction('conversions', 'readwrite');
+      let store = tx.objectStore('conversions');
+      store.put(conversion);
+    });
+  }
+
+  // get list of currecies from IDB
+  _getConversionFromIDB(query) {
+    const appController = this;
+    return new Promise((resolve, reject) => {
+      return appController._dbPromise.then(db => {
+        if (!db) return;
+
+        let tx = db.transaction('conversions').objectStore('conversions');
+        return tx.get(query).then(obj => resolve(obj));
+      });
+    }).then(response => response);
+  }
+
+  // get a conversion rate from IDB
+  _getCurrenciesFromIDB() {
+    const appController = this;
+    appController._dbPromise.then(db => {
+      if (!db) return;
+
+      let tx = db.transaction('currencies').objectStore('currencies');
+      return tx.getAll().then(currencies => {
+        appController._addCurrenciesToView(currencies);
+        appController._fetchConversionRate();
+      });
+    });
+  }
+
+  // add list of currencies to view
+  _addCurrenciesToView(currencies) {
+    const appController = this;
+    for (let currency of currencies) {
+      const option1 = document.createElement('option');
+      option1.text = view.input1Label.text = currency.name;
+      view.currencyList1.add(option1);
+    }
+    for (let currency of currencies.reverse()) {
+      const option2 = document.createElement('option');
+      option2.text = view.input2Label.text = currency.name;
+      view.currencyList2.add(option2);
+    }
+    appController._userInteraction();
   }
 
   // apply conversion rate to inputs
@@ -176,64 +221,6 @@ class ApplicationController {
         .currencyName
     }`;
   }
-  // store a conversion rate on IDB
-  _storeConversionInIDB(conversion) {
-    this._dbPromise.then(db => {
-      if (!db) return;
-
-      let tx = db.transaction('conversions', 'readwrite');
-      let store = tx.objectStore('conversions');
-      // remove all previously stored currencies
-      store.openCursor(null).then(function deleteConversion(cursor) {
-        if (!cursor) return;
-        if (cursor.value.id == conversion.id) cursor.delete();
-        return cursor.continue().then(deleteConversion);
-      });
-      store.put(conversion);
-    });
-  }
-
-  // get list of currecies from IDB
-  _getCurrenciesFromIDB(query) {
-    const appController = this;
-    appController._dbPromise.then(db => {
-      if (!db) return;
-
-      let tx = db.transaction('conversions').objectStore('conversions');
-      return tx.getOne(query).then(conversion => {
-        appController._applyConversionRate(conversion.val, query);
-      });
-    });
-  }
-
-  // get a conversion rate from IDB
-  _getConversionFromIDB() {
-    const appController = this;
-    appController._dbPromise.then(db => {
-      if (!db) return;
-
-      let tx = db.transaction('currencies').objectStore('currencies');
-      return tx.getAll().then(currencies => {
-        appController._addCurrenciesToView(currencies);
-      });
-    });
-  }
-
-  // add list of currencies to view
-  _addCurrenciesToView(currencies) {
-    for (let currency of currencies) {
-      const option1 = document.createElement('option');
-      option1.text = view.input1Label.text = currency.name;
-      view.currencyList1.add(option1);
-    }
-    for (let currency of currencies.reverse()) {
-      const option2 = document.createElement('option');
-      option2.text = view.input2Label.text = currency.name;
-      view.currencyList2.add(option2);
-    }
-    this._userInteraction();
-  }
-
   // monitor network status
   _monitorNetwork() {
     window.addEventListener('online', this._updateNetworkStatus);
